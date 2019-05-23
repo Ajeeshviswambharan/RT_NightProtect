1648e3fd967cc016f6e11f2ee159f2805aa810c7
Ajeesh <ajeeshavi@gmail.com>
2019-05-23 09:10:21 +0900

    [artf1005] Structre Declared

---
+++ OS_based/Rte_Types.h
@@ -0,0 +1,9 @@
+extern D_Detils(Day_B_Day daily);
+
+typedef struct
+{
+    int date;
+    float expense; 
+
+}Intial_Detils;
+extern Intial_Detils Day_B_Day;
--- OS_based/main.c
+++ OS_based/main.c
@@ -1,11 +1,12 @@
 #include"main.h"
+#include"Rte_Types.h"
 #include<stdio.h>
 
 void main()
 {
 	os_init();
 	os_shedule();
-
+ //    D_Detils(Intial_Detils daily);
 	var = 1;
 		printf("Main Program!!!!!!\n");
 		printf("Main Program_Work_started!!!!!!!\n");
