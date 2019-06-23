370948cfc9a8ff34922b537c12c00030b6c5127a
Ajeesh <ajeeshavi@gmail.com>
2019-06-24 00:14:10 +0900

    [artf2013] For testing final

--- OS_based/main.c
+++ OS_based/main.c
@@ -31,29 +31,29 @@ void main()
 	printf("PLEASE ENTER YOUR CHOICE \n");
 	printf("   _________________________________________________\n");
 	printf(" PRESS\n");
-	printf(" 1-FOR UPDATING BANK WITHDROWAL AND DAILY BILLS\n");
-	printf(" 2-FOR UPDATING DAILY BILLS\n");
-	printf(" 3-FOR UPDATING BANK AMOUNT AND WIDROW AMOUNT \n");
-	printf(" 4-FOR UPDATING CREDIT CARD WITHDROWAL\n");
-	printf(" 5-FOR DISPLAY LAST DAY TRANSACTION\n");
-	printf(" 6-FOR DISPLAY LAST MONTH TRANSACTION \n");
+	//printf(" 1-FOR UPDATING BANK WITHDROWAL AND DAILY BILLS\n");
+	printf(" 1-FOR UPDATING DAILY BILLS\n");
+	printf(" 2-FOR UPDATING BANK AMOUNT AND WIDROW AMOUNT \n");
+	printf(" 3-FOR UPDATING CREDIT CARD WITHDROWAL\n");
+	printf(" 4-FOR DISPLAY LAST DAY TRANSACTION\n");
+	printf(" 5-FOR DISPLAY LAST MONTH TRANSACTION \n");
 	printf("   _________________________________________________\n");
 	scanf("%d", &choice_get);
 	switch (choice_get)
 	{
-	case 1:
-		printf(" PLEASE UPDATE WITHDROWAL AMOUNT AND DAILY BILLS\n");
+//	case 1:
+//		printf(" PLEASE UPDATE WITHDROWAL AMOUNT AND DAILY BILLS\n");
 		
-		daily1.bdupdate = 0;
-		date(&daily1);
-		if (terminate)
-			break;
-		withdrwal(&daily1);
-		D_Detils(&daily1);
-		sum_vages(&daily1);
-		updated_balance(&daily1);
-		break;
-	case 2:
+//		daily1.bdupdate = 0;
+//		date(&daily1);
+//		if (terminate)
+//			break;
+//		withdrwal(&daily1);
+//		D_Detils(&daily1);
+//		sum_vages(&daily1);
+//		updated_balance(&daily1);
+//		break;
+	case 1:
 		printf(" PLEASE UPDAYE DAILY BILLS\n");
 		daily1.bdupdate = 0;
 		date(&daily1);
@@ -63,7 +63,7 @@ void main()
 		sum_vages(&daily1);
 		updated_balance(&daily1);
 		break;
-	case 3:
+	case 2:
 		printf(" PLEASE UPDATE THE NEW AMOUNT CREDITED AND WIDROW AMOUNT \n");
 		daily1.bdupdate = 0;
 		daily1.total = 0;
@@ -74,7 +74,7 @@ void main()
 		withdrwal(&daily1);
 		updated_balance(&daily1);
 		break;
-	case 4:
+	case 3:
 		printf(" PLEASE UPDATE THE CREDIT CARD WITHDROWAL AMOUNT \n");
 		daily1.bdupdate = 0;
 		daily1.total = 0;
@@ -84,13 +84,13 @@ void main()
 		bank_Dupdate(&daily1);
 		updated_balance(&daily1);
 		break;
-	case 5:
+	case 4:
 		printf(" LAST DAY DETAILS\n");
 		d_details(&daily1);
 		break;
-	case 6:
+	case 5:
 		printf(" LAST MONTH TRNSACTION DETAILS\n");
-		printf(" PLEAASE ENTER THE MONTH IN MM FORMAT\n");
+		printf(" PLEASE ENTER THE MONTH IN MM FORMAT\n");
 		database_read_lastmonth(&daily1);
 		break;
 	default:
